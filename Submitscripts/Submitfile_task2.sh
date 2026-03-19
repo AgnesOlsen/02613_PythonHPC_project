5fe09bac76f119cfbf7918ecbb72dc439e748f21
sinegjensen <signegyberg@gmail.com>
2026-03-19 14:10:25 +0100

    Submitscript task 2

---
+++ Submitscripts/Submitfile_task2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#BSUB -J Task2
+#BSUB -q hpc
+#BSUB -W 20
+#BSUB -R "rusage[mem=10GB]"
+#BSUB -R "select[model==XeonGold6226R]"
+#BSUB -n 1
+#BSUB -R "span[hosts=1]"
+#BSUB -o Task2_%J.out
+#BSUB -e Task2_%J.err
+
+# Initialize Python environment
+source /dtu/projects/02613_2025/conda/conda_init.sh
+conda activate 02613
+
+time python Original_python_script.py 10
+time python Original_python_script.py 15
+time python Original_python_script.py 20
+
