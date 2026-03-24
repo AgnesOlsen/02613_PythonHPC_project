15784b871856d8a2c3a9a28ea25b07ec32ffe036
AgnesOlsen <s224381@dtu.dk>
2026-03-24 11:52:21 +0100

    time_stats in task5 batched

--- Submitscripts/Submitfile_task5.sh
+++ Submitscripts/Submitfile_task5.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-#BSUB -J Task5
+#BSUB -J 5016Task5
 #BSUB -q hpc
-#BSUB -W 10
-#BSUB -R "rusage[mem=1GB]"
+#BSUB -W 120
+#BSUB -R "rusage[mem=400MB]"
 #BSUB -R "select[model==XeonGold6226R]"
-#BSUB -n 4
+#BSUB -n 16
 #BSUB -R "span[hosts=1]"
-#BSUB -o outputfiles/Task5_%J.out
-#BSUB -e outputfiles/Task5_%J.err
+#BSUB -o outputfiles/5016Task5_%J.out
+#BSUB -e outputfiles/5016Task5_%J.err
 
 # Initialize Python environment
 source /dtu/projects/02613_2025/conda/conda_init.sh
-conda activate 02613
-
-python src/parallelize.py 5
+conda activate 02613 
+python src/Task5.py 50
