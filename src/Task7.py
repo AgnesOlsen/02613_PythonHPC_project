123ec12e820fa4bea3f881667d08578544f2f97c
sinegjensen <signegyberg@gmail.com>
2026-04-29 12:17:37 +0200

    task 7

--- src/Task7.py
+++ src/Task7.py
@@ -3,7 +3,9 @@ import sys
 
 import numpy as np
 from numba import jit
+
 import time
+import matplotlib.pyplot as plt
 
 def load_data(load_dir, bid):
     SIZE = 512
@@ -15,20 +17,22 @@ def load_data(load_dir, bid):
 @jit(nopython=True)
 def jacobi(u, interior_mask, max_iter, atol=1e-6):
     u = np.copy(u)
-
-    for i in range(max_iter):
+    u_new = np.copy(u)
+    for _ in range(max_iter):
         # Compute average of left, right, up and down neighbors, see eq. (1)
-        u_new = 0.25 * (u[1:-1, :-2] + u[1:-1, 2:] + u[:-2, 1:-1] + u[2:, 1:-1])
-        u_new_interior = np.zeros_like(u_new)
-        delta = np.zeros_like(u_new)
+        max_delta = 0
         for i in range(u_new.shape[0]): # går gennem alle rækkerne
             for j in range(u_new.shape[1]): # går gennem hvert element i rækkerne
-                if interior_mask[i,j]:
-                    u_new_interior[i,j] = u_new[i,j]
-                    delta[i,j] = u[1:-1, 1:-1][i,j] - u_new_interior[i,j]
-                    u[1:-1, 1:-1][i,j] = u_new_interior[i,j]
-        
-        max_delta = np.abs(delta).max()
+                if interior_mask[i-1,j-1]:
+                    u_new[i, j] = 0.25 * (
+                    u[i, j - 1] +  
+                    u[i, j + 1] +  
+                    u[i - 1, j] +  
+                    u[i + 1, j]    )
+                    delta = abs(u[i,j] - u_new[i,j])
+                    max_delta = max(max_delta,delta)
+
+        u, u_new = u_new, u
 
         if max_delta < atol:
             break
@@ -49,8 +53,30 @@ def summary_stats(u, interior_mask):
     }
 
 
+#### Have to comopile kernel 1 time before timing - so we need to load building ID's etc to test. 
+
+LOAD_DIR = '/dtu/projects/02613_2025/data/modified_swiss_dwellings/'
+with open(join(LOAD_DIR, 'building_ids.txt'), 'r') as f:
+    building_ids = f.read().splitlines()
+building_ids = building_ids[:1]
+
+all_u0 = np.empty((1, 514, 514))
+all_interior_mask = np.empty((1, 512, 512), dtype='bool')
+for i, bid in enumerate(building_ids):
+    u0, interior_mask = load_data(LOAD_DIR, bid)
+    all_u0[i] = u0
+    all_interior_mask[i] = interior_mask
+
+# Run jacobi iterations for each floor plan
+MAX_ITER = 20_000
+ABS_TOL = 1e-4
+
+all_u = np.empty_like(all_u0)
+for i, (u0, interior_mask) in enumerate(zip(all_u0, all_interior_mask)):
+    u = jacobi(u0, interior_mask, MAX_ITER, ABS_TOL)
+    all_u[i] = u
+
 if __name__ == '__main__':
-    t = time.time()
     # Load data
     LOAD_DIR = '/dtu/projects/02613_2025/data/modified_swiss_dwellings/'
     with open(join(LOAD_DIR, 'building_ids.txt'), 'r') as f:
@@ -69,19 +95,17 @@ if __name__ == '__main__':
         u0, interior_mask = load_data(LOAD_DIR, bid)
         all_u0[i] = u0
         all_interior_mask[i] = interior_mask
-    t = time.time() - t
+
+
     # Run jacobi iterations for each floor plan
     MAX_ITER = 20_000
     ABS_TOL = 1e-4
-
+    start_time = time.time()
     all_u = np.empty_like(all_u0)
     for i, (u0, interior_mask) in enumerate(zip(all_u0, all_interior_mask)):
-        if i == 1:
-            t2 = time.time()
         u = jacobi(u0, interior_mask, MAX_ITER, ABS_TOL)
         all_u[i] = u
-        print("1 image done")
-    t2 = time.time() - t2
+    print(f"Time for {N} is {time.time()-start_time}")
 
     # Print summary statistics in CSV format
     stat_keys = ['mean_temp', 'std_temp', 'pct_above_18', 'pct_below_15']
@@ -90,4 +114,12 @@ if __name__ == '__main__':
         stats = summary_stats(u, interior_mask)
         print(f"{bid},", ", ".join(str(stats[k]) for k in stat_keys))
 
-    print("total time = ", t + t2)
+fig, axs = plt.subplots(1, 4, figsize=(16, 6), constrained_layout=True)
+
+for i in range(4):
+    axs[i].imshow(all_u[i])
+    axs[i].set_title(f"Building {building_ids[i]}\nSimulation Results", fontsize=10, pad=6)
+    axs[i].axis("off")
+fig.suptitle("Visualization of First Four Buildings", fontsize=14)
+path_save = join('figures',"Task7_evn2026.png")
+plt.savefig(path_save,dpi=300, bbox_inches="tight")
